11e0dbc0a25062a5e74e8814c20f53d922a8eaf6
Marco Guillen <marco.guillen@cto.ai>
2023-05-08 20:50:31 -0500

    PROD-2741: Send x-ops-mechanism-header

---
+++ src/scripts/send-event.sh
@@ -0,0 +1,15 @@
+curl --location --request POST "https://events.stg.cto.ai" \
+  --header "`echo 'Authorization: Bearer' <<parameters.token>>`"\
+  --header 'Content-Type: application/json'\
+  --header "x-ops-mechanism: circle-ci-${VCS}"\
+  --data "{
+    \"repo\":\"<<parameters.repo>>\",
+    \"branch\":\"<<parameters.branch>>\",
+    \"commit\":\"<<parameters.commit>>\",
+    \"image\":\"<<parameters.image>>\",
+    \"environment\":\"<<parameters.environment>>\",
+    \"event_name\":\"<<parameters.event_name>>\",
+    \"event_action\":\"<<parameters.event_action>>\",
+    \"team_id\":\"<<parameters.team_id>>\",
+    \"meta\":{ \"user\":\"<<parameters.user>>\", \"origin\":\"<<parameters.origin>>\" }
+}"
