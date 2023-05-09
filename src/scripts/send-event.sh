82ca38618b13f7b5f07530b82098295ba34509f7
Marco Guillen <marco.guillen@cto.ai>
2023-05-08 21:43:23 -0500

    PROD-2741: Send x-ops-mechanism-header

--- src/scripts/send-event.sh
+++ src/scripts/send-event.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
-echo  "1  'Authorization: Bearer' <<parameters.token>>"
-echo  "2 x-ops-mechanism: circle-ci-${VCS}"
-echo  "3 \"meta\":{ \"user\":\"<<parameters.user>>\", \"origin\":\"<<parameters.origin>>\""
-
 curl --location --request POST "https://events.stg.cto.ai" \
-  --header "echo 'Authorization: Bearer' <<parameters.token>>"\
+  --header "echo 'Authorization: Bearer' ${TOKEN}"\
   --header 'Content-Type: application/json'\
   --header "x-ops-mechanism: circle-ci-${VCS}"\
   --data "{
-    \"repo\":\"<<parameters.repo>>\",
-    \"branch\":\"<<parameters.branch>>\",
-    \"commit\":\"<<parameters.commit>>\",
-    \"image\":\"<<parameters.image>>\",
-    \"environment\":\"<<parameters.environment>>\",
-    \"event_name\":\"<<parameters.event_name>>\",
-    \"event_action\":\"<<parameters.event_action>>\",
-    \"team_id\":\"<<parameters.team_id>>\",
-    \"meta\":{ \"user\":\"<<parameters.user>>\", \"origin\":\"<<parameters.origin>>\" }
+    \"repo\":\"${REPO}\",
+    \"branch\":\"${BRANCH}\",
+    \"commit\":\"${COMMIT}\",
+    \"image\":\"${IMAGE}\",
+    \"environment\":\"${ENVIRONMENT}\",
+    \"event_name\":\"${EVENT_NAME}\",
+    \"event_action\":\"${EVENT_ACTION}\",
+    \"team_id\":\"${TEAM_ID}\",
+    \"meta\":{ \"user\":\"${USER}\", \"origin\":\"${ORIGIN}\" }
 }"
